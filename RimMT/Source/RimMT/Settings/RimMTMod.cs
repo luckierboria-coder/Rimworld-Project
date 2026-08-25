acdc938acd69c99bd930f12291af6e6c6860d5ef
luckierboria-coder <luckierboria@gmail.com>
2026-08-25 20:06:43 +0800

    RimMT V0.4.3: expose path snapshot worker toggle

--- RimMT/Source/RimMT/Settings/RimMTMod.cs
+++ RimMT/Source/RimMT/Settings/RimMTMod.cs
@@ -6,24 +6,45 @@ namespace RimMT
     public sealed class RimMTMod : Mod
     {
         public static RimMTSettings Settings;
-        public RimMTMod(ModContentPack content):base(content) { Settings=GetSettings<RimMTSettings>(); RimMTRuntime.ApplySettings(Settings); }
+
+        public RimMTMod(ModContentPack content) : base(content)
+        {
+            Settings = GetSettings<RimMTSettings>();
+            RimMTRuntime.ApplySettings(Settings);
+        }
+
         public override string SettingsCategory() { return "RimMT"; }
+
         public override void DoSettingsWindowContents(Rect inRect)
         {
-            Listing_Standard listing=new Listing_Standard(); listing.Begin(inRect);
-            listing.Label("RimMT_SettingsIntro".Translate()); listing.GapLine();
-            listing.CheckboxLabeled("RimMT_AdaptiveBurst".Translate(),ref Settings.AdaptiveBurst,"RimMT_AdaptiveBurstDesc".Translate());
-            listing.CheckboxLabeled("RimMT_HotPathDiagnostics".Translate(),ref Settings.HotPathDiagnostics,"RimMT_HotPathDiagnosticsDesc".Translate());
+            Listing_Standard listing = new Listing_Standard();
+            listing.Begin(inRect);
+            listing.Label("RimMT_SettingsIntro".Translate());
             listing.GapLine();
-            listing.CheckboxLabeled("RimMT_TextCache".Translate(),ref Settings.TextCache,"RimMT_TextCacheDesc".Translate());
-            listing.CheckboxLabeled("RimMT_OverlayCache".Translate(),ref Settings.OverlayCache,"RimMT_OverlayCacheDesc".Translate());
-            listing.Label("RimMT_OverlayRefresh".Translate(Settings.OverlayRefreshFrames)); Settings.OverlayRefreshFrames=(int)listing.Slider(Settings.OverlayRefreshFrames,5f,120f);
-            listing.GapLine(); listing.Label("RimMT_Experimental".Translate());
-            listing.CheckboxLabeled("RimMT_ReachNoCache".Translate(),ref Settings.ReachNoCache,"RimMT_ReachNoCacheDesc".Translate());
-            listing.Label("RimMT_ReachTtl".Translate(Settings.ReachNoCacheTtl)); Settings.ReachNoCacheTtl=(int)listing.Slider(Settings.ReachNoCacheTtl,5f,60f);
-            listing.GapLine(); if(listing.ButtonText("RimMT_LogReport".Translate())) RimMTDiagnostics.LogStartupReport(); if(listing.ButtonText("RimMT_RunSelfTest".Translate())) RimMTDiagnostics.RunWorkerSelfTest();
-            listing.End(); RimMTRuntime.ApplySettings(Settings);
+            listing.CheckboxLabeled("RimMT_AdaptiveBurst".Translate(), ref Settings.AdaptiveBurst, "RimMT_AdaptiveBurstDesc".Translate());
+            listing.CheckboxLabeled("RimMT_HotPathDiagnostics".Translate(), ref Settings.HotPathDiagnostics, "RimMT_HotPathDiagnosticsDesc".Translate());
+            listing.GapLine();
+            listing.CheckboxLabeled("RimMT_TextCache".Translate(), ref Settings.TextCache, "RimMT_TextCacheDesc".Translate());
+            listing.CheckboxLabeled("RimMT_OverlayCache".Translate(), ref Settings.OverlayCache, "RimMT_OverlayCacheDesc".Translate());
+            listing.Label("RimMT_OverlayRefresh".Translate(Settings.OverlayRefreshFrames));
+            Settings.OverlayRefreshFrames = (int)listing.Slider(Settings.OverlayRefreshFrames, 5f, 120f);
+            listing.GapLine();
+            listing.Label("RimMT_Experimental".Translate());
+            listing.CheckboxLabeled("RimMT_PathSnapshotWorker".Translate(), ref Settings.PathSnapshotWorker, "RimMT_PathSnapshotWorkerDesc".Translate());
+            listing.CheckboxLabeled("RimMT_ReachNoCache".Translate(), ref Settings.ReachNoCache, "RimMT_ReachNoCacheDesc".Translate());
+            listing.Label("RimMT_ReachTtl".Translate(Settings.ReachNoCacheTtl));
+            Settings.ReachNoCacheTtl = (int)listing.Slider(Settings.ReachNoCacheTtl, 5f, 60f);
+            listing.GapLine();
+            if (listing.ButtonText("RimMT_LogReport".Translate())) RimMTDiagnostics.LogStartupReport();
+            if (listing.ButtonText("RimMT_RunSelfTest".Translate())) RimMTDiagnostics.RunWorkerSelfTest();
+            listing.End();
+            RimMTRuntime.ApplySettings(Settings);
         }
-        public override void WriteSettings() { base.WriteSettings(); RimMTRuntime.ApplySettings(Settings); }
+
+        public override void WriteSettings()
+        {
+            base.WriteSettings();
+            RimMTRuntime.ApplySettings(Settings);
+        }
     }
 }
