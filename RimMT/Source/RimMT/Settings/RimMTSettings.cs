72a7ac22213f30a7bf7f7682da43bb95e8290d12
luckierboria-coder <luckierboria@gmail.com>
2026-08-25 20:06:32 +0800

    RimMT V0.4.3: add path snapshot worker setting

--- RimMT/Source/RimMT/Settings/RimMTSettings.cs
+++ RimMT/Source/RimMT/Settings/RimMTSettings.cs
@@ -11,19 +11,27 @@ namespace RimMT
         public int ReachNoCacheTtl = 20;
         public bool AdaptiveBurst = true;
         public bool HotPathDiagnostics = true;
+        public bool PathSnapshotWorker = true;
 
         public override void ExposeData()
         {
-            Scribe_Values.Look(ref TextCache,"textCache",true);
-            Scribe_Values.Look(ref OverlayCache,"overlayCache",true);
-            Scribe_Values.Look(ref OverlayRefreshFrames,"overlayRefreshFrames",30);
-            Scribe_Values.Look(ref ReachNoCache,"reachNoCache",false);
-            Scribe_Values.Look(ref ReachNoCacheTtl,"reachNoCacheTtl",20);
-            Scribe_Values.Look(ref AdaptiveBurst,"adaptiveBurst",true);
-            Scribe_Values.Look(ref HotPathDiagnostics,"hotPathDiagnostics",true);
-            OverlayRefreshFrames=Clamp(OverlayRefreshFrames,5,120);
-            ReachNoCacheTtl=Clamp(ReachNoCacheTtl,5,60);
+            Scribe_Values.Look(ref TextCache, "textCache", true);
+            Scribe_Values.Look(ref OverlayCache, "overlayCache", true);
+            Scribe_Values.Look(ref OverlayRefreshFrames, "overlayRefreshFrames", 30);
+            Scribe_Values.Look(ref ReachNoCache, "reachNoCache", false);
+            Scribe_Values.Look(ref ReachNoCacheTtl, "reachNoCacheTtl", 20);
+            Scribe_Values.Look(ref AdaptiveBurst, "adaptiveBurst", true);
+            Scribe_Values.Look(ref HotPathDiagnostics, "hotPathDiagnostics", true);
+            Scribe_Values.Look(ref PathSnapshotWorker, "pathSnapshotWorker", true);
+            OverlayRefreshFrames = Clamp(OverlayRefreshFrames, 5, 120);
+            ReachNoCacheTtl = Clamp(ReachNoCacheTtl, 5, 60);
         }
-        private static int Clamp(int value,int min,int max) { if(value<min)return min; if(value>max)return max; return value; }
+
+        private static int Clamp(int value, int min, int max)
+        {
+            if (value < min) return min;
+            if (value > max) return max;
+            return value;
+        }
     }
 }
