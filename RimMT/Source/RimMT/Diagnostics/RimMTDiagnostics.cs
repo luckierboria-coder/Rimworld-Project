bd61ba950c9eae802eb57305d648825596671c56
luckierboria-coder <luckierboria@gmail.com>
2026-08-26 18:09:06 +0800

    RimMT V0.4.10: report single-call partition metrics

--- RimMT/Source/RimMT/Diagnostics/RimMTDiagnostics.cs
+++ RimMT/Source/RimMT/Diagnostics/RimMTDiagnostics.cs
@@ -84,6 +84,7 @@ namespace RimMT
             sb.AppendLine("Reach NO cache: hits=" + ReachabilityNoCache.Hits + ", stores=" + ReachabilityNoCache.Stores + ", topologyGen=" + ReachabilityNoCache.TopologyGeneration);
             sb.AppendLine(HaulWorkAccelerator.Summary());
             sb.AppendLine(GlobalHaulAccelerator.Summary());
+            sb.AppendLine(SingleCallCandidatePartition.Summary());
             sb.AppendLine(PathGridInvalidation.Summary());
             sb.AppendLine(PathSnapshotSafetyPatches.Summary());
             sb.AppendLine(PathSnapshotWorker.Summary());
