47c187cd18b8e6cd9c399e46267df06715a93239
luckierboria-coder <luckierboria@gmail.com>
2026-08-26 16:06:12 +0800

    RimMT V0.4.7: report global haul fast-path metrics

--- RimMT/Source/RimMT/Diagnostics/RimMTDiagnostics.cs
+++ RimMT/Source/RimMT/Diagnostics/RimMTDiagnostics.cs
@@ -83,6 +83,7 @@ namespace RimMT
             sb.AppendLine("Overlay cache: sourceScans=" + ThingOverlayCache.SourceScans + ", cachedFrames=" + ThingOverlayCache.CachedFrames);
             sb.AppendLine("Reach NO cache: hits=" + ReachabilityNoCache.Hits + ", stores=" + ReachabilityNoCache.Stores + ", topologyGen=" + ReachabilityNoCache.TopologyGeneration);
             sb.AppendLine(HaulWorkAccelerator.Summary());
+            sb.AppendLine(GlobalHaulAccelerator.Summary());
             sb.AppendLine(PathGridInvalidation.Summary());
             sb.AppendLine(PathSnapshotSafetyPatches.Summary());
             sb.AppendLine(PathSnapshotWorker.Summary());
