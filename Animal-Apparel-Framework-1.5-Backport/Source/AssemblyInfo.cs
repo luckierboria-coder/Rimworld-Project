cc768502bbe5744ff4bcf96f6c2bb05421ff6748
luckierboria-coder <luckierboria@gmail.com>
2026-08-30 20:25:54 +0800

    AAF15: add assembly metadata

---
+++ Animal-Apparel-Framework-1.5-Backport/Source/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using System.Reflection;
+[assembly: AssemblyTitle("Animal Apparel Framework 1.5 Backport")]
+[assembly: AssemblyDescription("RimWorld 1.5.4063 backport of Animal Apparel: Framework")]
+[assembly: AssemblyVersion("1.5.0.0")]
+[assembly: AssemblyFileVersion("1.5.0.0")]
