3f6520a0544bdc359d352e07234e19d4e10a844e
luckierboria-coder <luckierboria@gmail.com>
2026-08-31 15:18:14 +0800

    RimFG: add fixed-size native ABI

---
+++ RimFG/Source/Managed/NativeInterop.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Runtime.InteropServices;
+
+namespace RimFG
+{
+    [StructLayout(LayoutKind.Sequential, Pack = 4)]
+    internal struct FrameMetadata
+    {
+        public uint abiVersion;
+        public uint frameIndex;
+        public int screenWidth;
+        public int screenHeight;
+        public float cameraX;
+        public float cameraY;
+        public float cameraZ;
+        public float orthographicSize;
+        public float unscaledDeltaTime;
+        public int paused;
+        public int gameSpeed;
+        public int hudRectCount;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 4)]
+    internal struct HudRect
+    {
+        public float x;
+        public float y;
+        public float width;
+        public float height;
+
+        public HudRect(float x, float y, float width, float height)
+        {
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+        }
+    }
+
+    internal static class NativeInterop
+    {
+        internal const uint AbiVersion = 1;
+        private const string DllName = "RimFG.Native";
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern IntPtr RimFG_GetRenderEventFunc();
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern void RimFG_SubmitFrameMetadata(ref FrameMetadata metadata);
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern void RimFG_SubmitHudRects([In] HudRect[] rects, int count);
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern int RimFG_IsD3D11Ready();
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern void RimFG_SetEnabled(int enabled);
+    }
+}
